b89c1b34412e6632cb789f06327636efa63f74c0
Sammie <lythekiet333@gmail.com>
2023-08-02 20:36:23 +0930

    Fix bugs can't use tag helpers
    Fixed bugs that views can't use tag helpers.
    Commented out the configuring method to remove the warnings in the project.

--- AmazonOrders-b2e6221b0546f0c65d8b41b79971d76aa3d2fb89/WebApplication1/wwwroot/js/site.js
+++ AmazonOrders-b2e6221b0546f0c65d8b41b79971d76aa3d2fb89/WebApplication1/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function descriptionTruncate(description) {
+    if (description.length > 100) {
+        return description.substring(0, 100) + '...';
+    }
+return description;
+}
